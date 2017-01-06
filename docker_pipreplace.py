383476db220a57d7de2e18f433b86c87cf5db20d
Juanjo Alvarez <juanjux@gmail.com>
2017-01-06 22:38:02 +0100

    terser code, more in line with what the script really does (not much)

--- docker_pipreplace.py
+++ docker_pipreplace.py
@@ -11,26 +11,19 @@ Dockerfile or this will be for nothing (ADD commands invalidate Docker
 image catching from that point on)
 """
 
-
 def main(reqfile='requirements.txt', dockerfile_template='Dockerfile.template',
         dockerfile_output='Dockerfile', pipcmd='pip2', mark='### PIPREPLACE ###'):
 
-    pipcmds = 'RUN pip install --upgrade pip\n'
-
-    # Open the requirements.txt file, generate pip
     with open(reqfile, 'r') as rf:
         reqlines = rf.readlines()
 
+    pipcmds = 'RUN pip install --upgrade pip\n'
     for requirement in reqlines:
         pipcmds += 'RUN pip install {}'.format(requirement)
 
     with open(dockerfile_template, 'r') as dt:
-        dt_text = dt.read()
-
-    dt_text = dt_text.replace(mark, pipcmds)
-
-    with open(dockerfile_output, 'w') as dout:
-        dout.write(dt_text)
+        with open(dockerfile_output, 'w') as dout:
+            dout.write(dt.read().replace(mark, pipcmds))
 
 
 if __name__ == '__main__':
